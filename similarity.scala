e95253abeddaf1066f4344241ed249f1b20c1c78
Ivens Portugal <ivensportugal@me.com>
2018-05-21 10:09:09 -0400

    first commit

---
+++ similarity.scala
@@ -0,0 +1,8 @@
+/* This app calculates the similarity between two trajectories (.gpx) */
+import org.apache.spark.sql.SparkSession
+
+object Similarity {
+  def main(args: Array[String]) {
+    println(s"Hello World!")
+  }
+}
