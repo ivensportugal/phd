19ab1104b4973d781ddce97d8ef3e87be68df3dc
Ivens <ivensportugal@me.com>
2022-07-18 16:42:12 -0300

    removing file pp

--- src/pp.py
+++
@@ -1,9 +0,0 @@
-f = open('../data/trucks/Trucks.txt', 'r')
-for line in f:
-  datapoint = line.split(';')
-  _id = datapoint[0]
-  f2 = open('../process/preprocessed/'+_id+'.txt', 'a')
-  f2.write(line)
-
-f.close()
-f2.close() 
