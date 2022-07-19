806652d1729a18bb1181aadcb632d5c823febe3f
Ivens <ivensportugal@me.com>
2022-07-19 14:02:20 -0300

    optimization lower triangle

--- src/similarity.py
+++ src/similarity.py
@@ -9,7 +9,9 @@ df = pd.read_csv('/Users/Ivens/Downloads/sizes.csv')
 df['Sizes'] = [[int(n) for n in x.strip('[]').split(',')] for x in df['Sizes']]
 
 # run fastdtw (use second method for tests)
-distances = [[fastdtw(x, y, dist=euclidean)[0] for y in df['Sizes']] for x in df['Sizes']]
+n = len(df['Sizes'])
+distances = [[fastdtw(df['Sizes'][i], df['Sizes'][j], dist=euclidean)[0] for j in range(i+1)] for i in range(n)]
+# distances = [[fastdtw(x, y, dist=euclidean)[0] for y in df['Sizes']] for x in df['Sizes']]
 # distances = [[fastdtw(x, y, dist=euclidean)[0] for y in df.iloc[0:10,1]] for x in df.iloc[0:10,1]]
 
 # add headers to the columns and lines
